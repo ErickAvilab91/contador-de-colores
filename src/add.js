4dab479a13df445c167c0003129f65bd040cbaeb
Erick Avila <erick.avila91@gmail.com>
2023-10-14 11:27:54 -0600

    contador de colores

--- src/add.js
+++ src/add.js
@@ -1,1 +1,14 @@
-import chalk from "chalk";
+import chalk from 'chalk';
+
+export const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+export const showResult = (num1, num2) => {
+    const result = add(num1, num2);
+    if (result >= 5) {
+        return chalk.green(result);
+    } else {
+        return chalk.red(result);
+    }
+}
